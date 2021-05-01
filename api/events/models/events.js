bb5f1982bf993c769292ca06782eeed843daf65d
laur80 <laur80raul@gmail.com>
2021-05-01 13:42:53 +0300

    inputsearch, addEvent, fix API_URL

--- api/events/models/events.js
+++ api/events/models/events.js
@@ -3,6 +3,7 @@
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
+ * .
  */
 
 const slugify = require("slugify");
